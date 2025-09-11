305b3cda20ca2960c2f57cece0c9eab09d7df382
cbajandas <cbajandas@decentdao.org>
2025-09-11 11:40:58 -0700

    Updated the staking page tests to actually test the staking page content. I hate it, because these changes might make these tests flaky thanks to needing to reload the page, but it also makes them way more useful than they were before.

--- tests/token-voting/staking/content-loads.test.ts
+++ tests/token-voting/staking/content-loads.test.ts
@@ -4,8 +4,6 @@ import { BaseSeleniumTest } from '../../base-selenium-test';
 import { By } from 'selenium-webdriver';
 import { pages } from '../../../config/pages';
 
-// This test is kind of garbage because it can only test the zero state of the page at the moment. Need to consider how to simulate connecting to a wallet to make this better.
-
 // Parse governance type from CLI args
 const governanceArg = process.argv.find(arg => arg.startsWith('--governance='));
 const governanceType = governanceArg ? governanceArg.split('=')[1].toLowerCase() : 'erc20';
@@ -15,7 +13,25 @@ BaseSeleniumTest.run(async (test) => {
   await test.start();
   const pagePath = `${pages['staking']}?dao=${getTestDao(governanceType).value}`;
   await test.driver!.get(appendFlagsToUrl(getBaseUrl() + pagePath));
-  // Wait for the Go to Settings button to appear
-  await test.waitForElement(By.xpath("//button[contains(., 'Go to Settings')]"));
-  console.log('Staking page loaded and Go to Settings button found.');
+  
+  // NOTE: This shouldn't be necessary, but works around an issue with the tab contents showing on first load
+  await test.driver!.sleep(5000);
+  await test.driver!.navigate().refresh();
+  await test.driver!.sleep(8000);
+
+  // Wait for the input field to appear
+  const inputField = await test.driver!.findElement(By.xpath("//input[@type='text'][1]"));
+  console.log('Staking page loaded and input field found.');
+  
+  // Enter a test value
+  const testValue = '123.45';
+  await inputField.sendKeys(testValue);
+  
+  // Assert the value was entered correctly
+  const enteredValue = await inputField.getAttribute('value');
+  if (enteredValue === testValue) {
+    console.log(`Successfully entered and verified value: ${testValue}`);
+  } else {
+    throw new Error(`Expected value '${testValue}' but got '${enteredValue}'`);
+  }
 }, test);
